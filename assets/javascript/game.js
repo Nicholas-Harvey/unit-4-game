714cb584fb800f3cd98d7e755f2323d487f54984
Nicholas Harvey <nicholas.harvey@gmail.com>
2018-06-17 10:49:42 -0600

    finished homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,87 @@
-var letters = "abcdefghijklmnopqrstuvwxyz".split("");
-var userGuess = null;
-var computerGuess = letters[Math.floor(math.random("") * letters.length)];
-var guessesLeft = 9;
-var guesses = [];
-var wins = 0;
-var losses = 0;
+$(document).ready(function () {
+    var startingScore = 0;
+    var wins = 0;
+    var losses = 0;
+    var currentScore = 0;
+    var randomNumber;
+    var scoreNeeded;
+    var agateValue;
+    var lapisValue;
+    var brazilianiteValue;
+    var labradoriteValue;
 
 
-document.ready(function(){
 
-});
-
-console.log("Wins" + wins + "Losses" + losses + "Guesses Left" + guessesLeft + "Guesses" + guesses + "Computer Guess" + computerGuess);
-doncument.onkeyup = function (event) {
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    if (guesses.indexOf(userGuess) < 0 && letters.indexOf(userGuess) >= 0) {
-
+    function startGame() {
+        agateValue = Math.floor(Math.random() * 12) + 1;
+        lapisValue = Math.floor(Math.random() * 12) + 1;
+        brazilianiteValue = Math.floor(Math.random() * 12) + 1;
+        labradoriteValue = Math.floor(Math.random() * 12) + 1;
+        randomNumber = Math.floor(Math.random() * 120) + 19;
+        $("#random-number").html(randomNumber);
     }
 
-    if (userGuess === computerGuess) {
-        wins++
-        console.log("win");
-        guessesLeft = 9;
-        guesses = [];
-        computerGuess = letters[Math.floor(math.random() * letters.length)];
-        console.log("Wins" + wins + "Losses" + losses + "Guesses Left" + guessesLeft + "Guesses" + guesses + "Computer Guess" + computerGuess);
-        document.getElementById("Win Count").innerHTML=wins;
-    }
-    if(guessesLeft == 0) {
-        losses ++;
-        console.log("Lose");
-        guessesLeft = 9;
-        guesses = [];
-        computerGuess = letters[Math.floor(math.random() * letters.length)];
-        console.log("Wins" + wins + "Loses" + losses + "Guesses Left" + guessesLeft + "Guesses" + guesses + "Computer Guess" + computerGuess);
-        document.getElementById("Loss Count").innerHTML=losses;
+    startGame();
+
+    function restartGame() {
+        startGame()
+        currentScore = 0;
+        $("#score-display").html(currentScore);
     };
-};
+
+
+
+
+    $("#agate-image").on("click", function () {
+        console.log("agate-value " + agateValue);
+        currentScore += agateValue;
+        $("#score-display").html(currentScore);
+        gameChecker();
+    });
+
+
+    $("#lapis-lazuli-image").on("click", function () {
+        console.log("lapis-lazuli-value " + lapisValue);
+        currentScore += lapisValue;
+        $("#score-display").html(currentScore);
+        gameChecker();
+    });
+
+    $("#brazilianite-image").on("click", function () {
+        console.log("brazilianite-value " + brazilianiteValue);
+        currentScore += brazilianiteValue;
+        $("#score-display").html(currentScore);
+        gameChecker();
+    });
+
+
+    $("#labradorite-image").on("click", function () {
+        console.log("labradorite-value " + brazilianiteValue);
+        currentScore += labradoriteValue;
+        $("#score-display").html(currentScore);
+        gameChecker();
+    });
+
+
+    console.log(
+        $(".start").on("click", function () {
+            scoreNeeded = Math.floor((Math.random() * 65) + 1);
+            $("#needed-display").html(scoreNeeded);
+            $("#score-display").empty();
+            $("#current-score").empty();
+
+        }));
+
+    function gameChecker() {
+        if (currentScore === scoreNeeded) {
+            wins++;
+            $("#wins-display ").html(wins);
+            restartGame();
+        };
+        if (currentScore > scoreNeeded) {
+            losses++;
+            $("#losses-display ").html(losses);
+            restartGame();
+        };
+    };
+});
